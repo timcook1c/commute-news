210caeadacf464f85a41ae010025b6cbf0a08433
timcook1c <tim.cook1@gmail.com>
2026-09-05 14:07:38 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "commute-v1";
+const CACHE_NAME = "commute-v2";
 const APP_SHELL = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
